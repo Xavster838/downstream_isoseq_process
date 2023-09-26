d5e75b4376c549c30e286bfe406aad86cfb680ae
Xavster838 <francisguitart@gmail.com>
2023-09-26 14:01:26 -0700

    fixed bugs in scripts/get_long_supported_isoforms.py

--- workflow/scripts/get_long_supported_isoforms.py
+++ workflow/scripts/get_long_supported_isoforms.py
@@ -7,7 +7,7 @@ from Bio.SeqFeature import SeqFeature, FeatureLocation
 canonical_bed12_path = snakemake.input['canonical_bed12'] 
 abundance_tbl_path = snakemake.input['abundance_tbl']
 ref_gene_mappings_bed_path = snakemake.input['ref_gene_mappings_bed']
-isoform_gff_path = snakemake.input['isoform_gff ']
+isoform_gff_path = snakemake.input['isoform_gff']
 
 #step 1: filter abundance tbl 
 MIN_ISO_COUNT = snakemake.params["min_abundance"]
@@ -18,7 +18,7 @@ abund_tbl = abund_tbl.loc[abund_tbl["count_fl"] > MIN_ISO_COUNT,:]
 #step 2 filter TBC gff with canonical gff
 #step 2a. load bed12
 bed_12_cols = ["contig", "start", "end", "aln", ".", "strand", "_start2", "_end2", "rgb", "n_exons", "exon_size", "exon_start"]
-bed12 = pd.read_csv(ref_mRNA_bed12_path, sep = "\t", usecols=list(range(12)), names = bed_12_cols)
+bed12 = pd.read_csv(canonical_bed12_path, sep = "\t", usecols=list(range(12)), names = bed_12_cols)
 bed12 = bed12.set_index("aln", drop = False)
 
 #load gff file
@@ -31,10 +31,11 @@ with open(isoform_gff_path, 'r') as file:
             continue
         columns = line.strip().split('\t')
         attributes = columns[8].strip().split(';')
+        attributes = [element.strip() for element in attributes if element]
         attribute_dict = {key_value.split('=')[0]: key_value.split('=')[1] for key_value in attributes}
         if columns[2] == "transcript":
-            if "nbis" in attribute_dict["ID"]: #not sure why, but gff has some extra annotations with an nbis- name in ID...
-                continue
+           # if "nbis" in attribute_dict["ID"]: #not sure why, but sometimes gff has some extra annotations with an nbis- name in ID...
+           #     continue
             new_transcript = pd.Series({"id" : attribute_dict["transcript_id"] , "start" : int(columns[3]), "end" : int(columns[4]), 
                                         "strand" : columns[6], "paralog" : attribute_dict["paralog"],
                                         "exon_start" : [], "exon_size" : []})
@@ -60,6 +61,7 @@ with open(isoform_gff_path, 'r') as file:
 #filter on length
 FLANK_TOLERANCE = snakemake.params["flank_tolerance"]
 length_filter_isos = pd.DataFrame(columns = ['id', 'start', 'end', 'strand', 'paralog', 'exon_start', 'exon_size'], )
+missed=0
 for transcript_id, row in trans_df.iterrows():
     if(row.paralog not in bed12.index):
         missed += 1
@@ -67,6 +69,9 @@ for transcript_id, row in trans_df.iterrows():
     cur_aln = bed12.loc[row.paralog]   
     if(row.start <= cur_aln.start + 100 and row.end >= cur_aln.end - 100):
         length_filter_isos = pd.concat([length_filter_isos, row.to_frame().T] )
+    else:
+        missed += 1
+
 #only take isoforms that fall in filtering of abundance_tbl
 #now take al these that also have enough copies
 keep_isos = pd.DataFrame(columns = ['id', 'start', 'end', 'strand', 'paralog', 'exon_start', 'exon_size', 'abundance'], )
@@ -86,5 +91,5 @@ for transcript_id, row in length_filter_isos.iterrows():
 keep_isos.to_csv(snakemake.output["keep_isos_tbl"], sep = "\t", header=True, index = False )
 #write list
 with open(snakemake.output["keep_isos_lst"], "w") as file:
-    for element in my_list:
+    for element in list(keep_isos.index):
         file.write(element + '\n')
